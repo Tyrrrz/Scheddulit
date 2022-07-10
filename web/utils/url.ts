c87aa0ccb4229e31f3c7a103b6823472be53e6f0
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-07-10 10:37:31 +0000

    Simplify `formatUrlWithQuery`

--- web/utils/url.ts
+++ web/utils/url.ts
@@ -3,8 +3,8 @@ export const formatUrlWithQuery = (url: string, params: { [key: string]: string 
 
   const searchParams = new URLSearchParams(search);
 
-  for (const key of Object.keys(params)) {
-    searchParams.set(key, params[key]);
+  for (const [key, value] of Object.entries(params)) {
+    searchParams.set(key, value);
   }
 
   return base + '?' + searchParams.toString();
